99aee103f3fe686365d70115790daab18e270ebb
jud-ie19 <owuorakinyijudith@gmail.com>
2022-02-18 21:48:21 +0300

    Add my file

---
+++ src/main.kt
@@ -0,0 +1,40 @@
+fun main(){
+    name()
+    modulus()
+    add(12 ,13 ,19, 45)
+    aboutmyself()
+}
+
+
+fun name(){
+    var name="judy"
+    println("hello "+ name)
+}
+
+fun modulus(){
+    var a= 34
+    var b= 12
+    var modulus= a%b
+    println(a%b)
+}
+
+fun add(num1: Int, num2: Int, num3 :Int, num4 :Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+
+}
+
+fun aboutmyself(){
+    println( "i am writer")
+
+}
+
+
+
+
+
+
+
+
+
+
